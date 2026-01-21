17352ce3c3b6343da5e1acb537913bb775bb5ac9
Shivamkumar85406688 <shivamloard123@gmail.com>
2026-01-21 20:59:59 +0530

    Implement error handling for negative input
    Added error handling for negative input in factorial calculation.

--- factoral.c
+++ factoral.c
@@ -3,14 +3,15 @@ int main(){
 	int n,factorial = 1, i = 1;
 	printf("Enter your number:");
 	scanf("%d",&n);
-	/*if(n<0){
+	if(n<0){
 		printf("Error");
-	}else{*/
+	}else{
 		while(i<=n){
 			factorial *=i;
 			i++;
 		}
 		printf("%d ",factorial);
-    //}
+    }
 	return 0;
 }
+
