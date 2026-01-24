2eba908e6edc40f4243a4e39ccaab2fdbd844990
SHIVAM_KUMAR_08 <shivamloard123@gmail.com>
2026-01-24 11:53:37 +0530

    Add files via upload

---
+++ RowCol.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main() {
+    int mat[2][2],i,j;
+    printf("Enter matrix Elements:");
+    for(i=0 ; i<2 ; i++)
+	{
+    	for(j=0 ; j<2 ; j++)
+		{
+    		scanf(" %d",&mat[i][j]);
+		}
+	}
+		printf(" matrix Elements:\n");
+		for(i=0 ; i<2 ; i++)
+	    {
+    	for(j=0 ; j<2 ; j++)
+		{
+    		printf("%d",mat[i][j]);
+		}
+		printf("\n");
+	}
+	return 0;
+}
+    
+
+    
+
